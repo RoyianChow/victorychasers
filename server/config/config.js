c7b64e84474652a984aa42ed9349c47d74b1b374
Royian Chowdhury <rchowd44@my.centennialcollege.ca>
2023-03-01 16:33:01 -0500

    commit

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,4 @@
-export const MongoURI = "mongodb://localhost:27017/media";
+//changed uri
+export const MongoURI = "mongodb+srv://rchowd:Shahadah123@midterm.dlgtgzr.mongodb.net/?retryWrites=true&w=majority";
 export const Secret = "someSecret";
 //test
--- server/controllers/books.js
+++ server/controllers/books.js
@@ -12,42 +12,96 @@ export function displayBookList(req, res, next) {
         res.render('index', { title: 'Book List', page: 'books/list', books: booksCollection });
     });
 }
-
 //  GET the Book Details page in order to add a new Book
 export function displayAddPage(req, res, next) {
-
-    /*****************
-    * ADD CODE HERE *
-    *****************/
+    // render the add page with a default empty book object
+    res.render('content/books/add', { title: 'Add a New Book', book: {} });
 }
 
 // POST process the Book Details page and create a new Book - CREATE
 export function processAddPage(req, res, next) {
+    // create a new book object from the request's body
+    let newBook = new booksModel({
+        name: req.body.name,
+        author: req.body.author,
+        published: req.body.published,
+        description: req.body.description,
+        price: req.body.price
+    });
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+    // save the new book to the database
+    newBook.save((err) => {
+        if (err) {
+            console.error(err);
+            res.end(err);
+        }
+        else {
+            // redirect to the book list page
+            res.redirect('/books/list');
+        }
+    });
 }
 
 // GET the Book Details page in order to edit an existing Book
 export function displayEditPage(req, res, next) {
+    // get the id from the request parameters
+    let id = req.params.id;
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
-
+    // find the book by id in the database
+    booksModel.findById(id, (err, book) => {
+        if (err) {
+            console.error(err);
+            res.end(err);
+        }
+        else {
+            // render the edit page with the book object
+            res.render('content/books/edit', { title: 'Edit Book', book: book });
+        }
+    });
 }
 
 // POST - process the information passed from the details form and update the document
 export function processEditPage(req, res, next) {
-    /*****************
-    * ADD CODE HERE *
-    *****************/
+    // get the id from the request parameters
+    let id = req.params.id;
+
+    // create a new book object with the updated values
+    let updatedBook = new booksModel({
+        _id: id,
+        name: req.body.name,
+        author: req.body.author,
+        published: req.body.published,
+        description: req.body.description,
+        price: req.body.price
+    });
+
+    // update the book in the database
+    booksModel.updateOne({ _id: id }, updatedBook, (err) => {
+        if (err) {
+            console.error(err);
+            res.end(err);
+        }
+        else {
+            // redirect to the book list page
+            res.redirect('/books/list');
+        }
+    });
 }
 
 // GET - process the delete by user id
 export function processDelete(req, res, next) {
-    /*****************
-  * ADD CODE HERE *
-  *****************/
+    // get the id from the request parameters
+    let id = req.params.id;
+
+    // remove the book from the database
+    booksModel.remove({ _id: id }, (err) => {
+        if (err) {
+            console.error(err);
+            res.end(err);
+        }
+        else {
+            // redirect to the book list page
+            res.redirect('/books/list');
+        }
+    });
 }
