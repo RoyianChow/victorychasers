a31fde25eb1528462d882664a70a4dcc76e1d7c5
Royian Chowdhury <rchowd44@my.centennialcollege.ca>
2023-03-14 12:25:54 -0400

    connected to mongodb, added routes, controllers and models

--- app/app.js
+++ app/app.js
@@ -1,19 +1,30 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
-import session  from 'express-session';
+import session from 'express-session';
 
 // ES2022 Modules fix for __dirname
 import path, {dirname} from 'path';
 import { fileURLToPath } from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+// Import Mongoose module
+import mongoose from 'mongoose';
+
 // Configuration Module
-import { Secret } from '../config/index.js';
+import { Secret, MongoURI } from '../config/index.js';
 
 // Import Routes
 import indexRouter from '../app/routes/index.js';
+import tournamentsRouter from '../app/routes/tournaments.js';
 
+// Complete DB Configuration
+mongoose.connect(MongoURI);
+const db = mongoose.connection;
+
+// Database Listeners
+db.on('open', () => console.log(`Connected to MongoDB at ${MongoURI} `));
+db.on('error', () => console.log("Mongo Connection Error"));
 
 // Instantiate the express application
 const app = express();
@@ -24,8 +35,6 @@ const app = express();
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
-
-
 // General Middlewares
 app.use(logger('dev'));
 app.use(express.json());
@@ -33,13 +42,23 @@ app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(session({
-    secret: Secret,
-    saveUninitialized: false,
-    resave: false
+secret: Secret,
+saveUninitialized: false,
+resave: false
 }));
 
-// Use Routes
-app.use('/',indexRouter);
+// Custom Middleware to check authentication
+function isLoggedIn(req, res, next) {
+if (req.isAuthenticated()) {
+// If user is authenticated, call the next middleware function
+return next();
+}
+// If user is not authenticated, redirect to login page
+res.redirect('/login');
+}
 
+// Use Routes
+app.use('/', indexRouter);
+app.use('/', isLoggedIn, tournamentsRouter);
 
 export default app;
