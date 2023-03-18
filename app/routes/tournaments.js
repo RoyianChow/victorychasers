129adee89722fd1aeb340df05b34eca5baf7be75
Royian Chowdhury <rchowd44@my.centennialcollege.ca>
2023-03-17 21:52:29 -0400

    patches

--- app/routes/tournaments.js
+++ app/routes/tournaments.js
@@ -1,20 +1,21 @@
 import {Router} from 'express';
 import { DisplayTournamentsAddPage, DisplayTournamentsEditPage, DisplayTournamentsList, ProcessTournamentsAddPage, ProcessTournamentsDelete, ProcessTournamentsEditPage } from '../controllers/tournaments.js';
+import { AuthGuard } from '../utils/index.js';
 
 const router = Router();
 
 // C reate
 router.get('/tournament-add', DisplayTournamentsAddPage);
-router.post('/tournament-add', ProcessTournamentsAddPage);
+router.post('/tournament-add', AuthGuard, ProcessTournamentsAddPage);
 // R ead
-router.get('/tournament-list', DisplayTournamentsList);
+router.get('/tournament-list', AuthGuard, DisplayTournamentsList);
 
 
 // U pdate
-router.get('/tournament-edit/:id', DisplayTournamentsEditPage);
-router.post('/tournament-edit/:id', ProcessTournamentsEditPage);
+router.get('/tournament-edit/:id',AuthGuard, DisplayTournamentsEditPage);
+router.post('/tournament-edit/:id',AuthGuard, ProcessTournamentsEditPage);
 
 // D elete
-router.get('/tournament-delete/:id', ProcessTournamentsDelete);
+router.get('/tournament-delete/:id', AuthGuard,ProcessTournamentsDelete);
 
 export default router; //
